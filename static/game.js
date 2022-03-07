793b31c60bbc40eae9005ca54b7a3c50c24cb626
electrolys <47481369+electrolys@users.noreply.github.com>
2022-03-07 14:20:05 -0600

    a

--- static/game.js
+++ static/game.js
@@ -130,23 +130,37 @@ var player = /** @class */ (function () {
     return player;
 }());
 var pl = new player();
+var vertbuf = /** @class */ (function () {
+    function vertbuf() {
+    }
+    return vertbuf;
+}());
 var gamestate = /** @class */ (function () {
     function gamestate() {
-        this.level = new array2d(1000);
+        this.level = new array2d(1002);
+        this.chunks = new array2d(50);
         var js = loadJSON("static/level.json");
         var inc = 0;
-        for (var i = 0; i < 1000; i++) {
-            for (var j = 0; j < 1000; j++) {
+        for (var i = 0; i < 1002; i++)
+            for (var j = 0; j < 1002; j++) {
+                this.level.s(i, j, -1);
+            }
+        for (var i = 1; i < 1001; i++) {
+            for (var j = 1; j < 1001; j++) {
                 this.level.s(i, j, js[inc]);
                 inc++;
             }
         }
-        this.vbo = gl.createBuffer();
-        //this.vao=gl.createVertexArray();
-        //gl.bindVertexArray(this.vao);
-        gl.bindBuffer(gl.ARRAY_BUFFER, this.vbo);
-        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([0, 0, 0, 0]), gl.STATIC_DRAW);
-        this.vsize = 0;
+        for (var i = 0; i < 50; i++)
+            for (var j = 0; j < 50; j++) {
+                this.chunks.s(i, j, new vertbuf);
+                this.chunks.g(i, j).vbo = gl.createBuffer();
+                //this.vao=gl.createVertexArray();
+                //gl.bindVertexArray(this.vao);
+                gl.bindBuffer(gl.ARRAY_BUFFER, this.chunks.g(i, j).vbo);
+                gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([0, 0, 0, 0]), gl.STATIC_DRAW);
+                this.chunks.g(i, j).vsize = 0;
+            }
         // gl.vertexAttribPointer(0, 2, gl.FLOAT, false, sizeof(1.1)*4, sizeof(1.1)*0);
         // gl.enableVertexAttribArray(0);
         // gl.vertexAttribPointer(1, 2, gl.FLOAT, false, sizeof(1.1)*4, sizeof(1.1)*2);
@@ -170,83 +184,98 @@ var gamestate = /** @class */ (function () {
         this.tex = loadTexture('static/test.png');
     }
     gamestate.prototype.update = function () {
-        var data = [];
-        for (var i = 1; i < 1000; i++)
-            for (var j = 1; j < 1000; j++) {
-                var o = 5;
-                if (this.level.g(i, j) > 0)
-                    o = 10;
-                else if (this.level.g(i + 1, j) > 0 && this.level.g(i, j + 1) > 0 && this.level.g(i - 1, j) > 0 && this.level.g(i, j - 1) > 0)
-                    o = 5;
-                else if (this.level.g(i + 1, j) > 0 && !(this.level.g(i - 1, j) > 0 || ((this.level.g(i, j + 1) > 0) != (this.level.g(i, j - 1) > 0))))
-                    o = 4;
-                else if (this.level.g(i - 1, j) > 0 && !(this.level.g(i + 1, j) > 0 || ((this.level.g(i, j + 1) > 0) != (this.level.g(i, j - 1) > 0))))
-                    o = 3;
-                else if (this.level.g(i, j + 1) > 0 && !(this.level.g(i, j - 1) > 0 || ((this.level.g(i + 1, j) > 0) != (this.level.g(i - 1, j) > 0))))
-                    o = 2;
-                else if (this.level.g(i, j - 1) > 0 && !(this.level.g(i, j + 1) > 0 || ((this.level.g(i + 1, j) > 0) != (this.level.g(i - 1, j) > 0))))
-                    o = 1;
-                else if ((this.level.g(i, j - 1) > 0) == (this.level.g(i, j + 1) > 0) && (this.level.g(i - 1, j) > 0) == (this.level.g(i + 1, j) > 0) && (this.level.g(i, j - 1) > 0) != (this.level.g(i - 1, j) > 0))
-                    o = 0;
-                else if (this.level.g(i + 1, j) > 0 || this.level.g(i - 1, j) > 0 || this.level.g(i, j + 1) > 0 || this.level.g(i, j - 1) > 0)
-                    o = 5;
-                else if ((((this.level.g(i + 1, j + 1) > 0) ? 1 : 0) + ((this.level.g(i - 1, j + 1) > 0) ? 1 : 0) + ((this.level.g(i + 1, j - 1) > 0) ? 1 : 0) + ((this.level.g(i - 1, j - 1) > 0) ? 1 : 0)) > 1)
-                    o = 5;
-                else if (this.level.g(i + 1, j + 1) > 0)
-                    o = 6;
-                else if (this.level.g(i - 1, j + 1) > 0)
-                    o = 7;
-                else if (this.level.g(i + 1, j - 1) > 0)
-                    o = 8;
-                else if (this.level.g(i - 1, j - 1) > 0)
-                    o = 9;
-                else
-                    o = 0;
-                var id = Math.abs(this.level.g(i, j)) - 1;
-                data.push(i);
-                data.push(j);
-                data.push(o + 0.01);
-                data.push(id + 0.01);
-                data.push(i);
-                data.push(j + 1);
-                data.push(o + 0.01);
-                data.push(id + 0.99);
-                data.push(i + 1);
-                data.push(j);
-                data.push(o + 0.99);
-                data.push(id + 0.01);
-                data.push(i + 1);
-                data.push(j + 1);
-                data.push(o + 0.99);
-                data.push(id + 0.99);
-                data.push(i + 1);
-                data.push(j);
-                data.push(o + 0.99);
-                data.push(id + 0.01);
-                data.push(i);
-                data.push(j + 1);
-                data.push(o + 0.01);
-                data.push(id + 0.99);
+        for (var it = 0; it < 50; it++)
+            for (var jt = 0; jt < 50; jt++) {
+                var data = [];
+                for (var i = 1 + it * 20; i < 1 + (it + 1) * 20; i++)
+                    for (var j = 1 + jt * 20; j < 1 + (jt + 1) * 20; j++) {
+                        var o = 5;
+                        if (this.level.g(i, j) > 0)
+                            o = 10;
+                        else if (this.level.g(i + 1, j) > 0 && this.level.g(i, j + 1) > 0 && this.level.g(i - 1, j) > 0 && this.level.g(i, j - 1) > 0)
+                            o = 5;
+                        else if (this.level.g(i + 1, j) > 0 && !(this.level.g(i - 1, j) > 0 || ((this.level.g(i, j + 1) > 0) != (this.level.g(i, j - 1) > 0))))
+                            o = 4;
+                        else if (this.level.g(i - 1, j) > 0 && !(this.level.g(i + 1, j) > 0 || ((this.level.g(i, j + 1) > 0) != (this.level.g(i, j - 1) > 0))))
+                            o = 3;
+                        else if (this.level.g(i, j + 1) > 0 && !(this.level.g(i, j - 1) > 0 || ((this.level.g(i + 1, j) > 0) != (this.level.g(i - 1, j) > 0))))
+                            o = 2;
+                        else if (this.level.g(i, j - 1) > 0 && !(this.level.g(i, j + 1) > 0 || ((this.level.g(i + 1, j) > 0) != (this.level.g(i - 1, j) > 0))))
+                            o = 1;
+                        else if ((this.level.g(i, j - 1) > 0) == (this.level.g(i, j + 1) > 0) && (this.level.g(i - 1, j) > 0) == (this.level.g(i + 1, j) > 0) && (this.level.g(i, j - 1) > 0) != (this.level.g(i - 1, j) > 0))
+                            o = 0;
+                        else if (this.level.g(i + 1, j) > 0 || this.level.g(i - 1, j) > 0 || this.level.g(i, j + 1) > 0 || this.level.g(i, j - 1) > 0)
+                            o = 5;
+                        else if ((((this.level.g(i + 1, j + 1) > 0) ? 1 : 0) + ((this.level.g(i - 1, j + 1) > 0) ? 1 : 0) + ((this.level.g(i + 1, j - 1) > 0) ? 1 : 0) + ((this.level.g(i - 1, j - 1) > 0) ? 1 : 0)) > 1)
+                            o = 5;
+                        else if (this.level.g(i + 1, j + 1) > 0)
+                            o = 6;
+                        else if (this.level.g(i - 1, j + 1) > 0)
+                            o = 7;
+                        else if (this.level.g(i + 1, j - 1) > 0)
+                            o = 8;
+                        else if (this.level.g(i - 1, j - 1) > 0)
+                            o = 9;
+                        else
+                            o = 0;
+                        var id = Math.abs(this.level.g(i, j)) - 1;
+                        data.push(i);
+                        data.push(j);
+                        data.push(o + 0.01);
+                        data.push(id + 0.01);
+                        data.push(i);
+                        data.push(j + 1);
+                        data.push(o + 0.01);
+                        data.push(id + 0.99);
+                        data.push(i + 1);
+                        data.push(j);
+                        data.push(o + 0.99);
+                        data.push(id + 0.01);
+                        data.push(i + 1);
+                        data.push(j + 1);
+                        data.push(o + 0.99);
+                        data.push(id + 0.99);
+                        data.push(i + 1);
+                        data.push(j);
+                        data.push(o + 0.99);
+                        data.push(id + 0.01);
+                        data.push(i);
+                        data.push(j + 1);
+                        data.push(o + 0.01);
+                        data.push(id + 0.99);
+                    }
+                gl.bindBuffer(gl.ARRAY_BUFFER, this.chunks.g(it, jt).vbo);
+                gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(data), gl.STATIC_DRAW);
+                this.chunks.g(it, jt).vsize = data.length / 4;
             }
-        gl.bindBuffer(gl.ARRAY_BUFFER, this.vbo);
-        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(data), gl.STATIC_DRAW);
         gl.bindBuffer(gl.ARRAY_BUFFER, null);
-        this.vsize = data.length / 4;
     };
     gamestate.prototype.render = function () {
         gl.useProgram(this.shader);
         gl.activeTexture(gl.TEXTURE0);
         gl.bindTexture(gl.TEXTURE_2D, this.tex);
-        gl.bindBuffer(gl.ARRAY_BUFFER, this.vbo);
-        var t = gl.getUniformLocation(this.shader, 'i');
-        gl.vertexAttribPointer(t, 4, gl.FLOAT, false, 0, 0);
-        gl.enableVertexAttribArray(t);
-        // gl.vertexAttribPointer(t2, 2, gl.FLOAT, false, 8*4, 8*2);
-        // gl.enableVertexAttribArray(t2);
         gl.uniform2f(gl.getUniformLocation(this.shader, 'off'), -pl.pos.x, -pl.pos.y);
         gl.uniform2f(gl.getUniformLocation(this.shader, 'scl'), 1 / 16, (1 / 16) * (canvas.width / canvas.height));
         gl.uniform2i(gl.getUniformLocation(this.shader, 'spnum'), 11, 2);
-        gl.drawArrays(gl.TRIANGLES, 0, this.vsize);
+        var stop = pl.pos.y - 16;
+        var sbottom = pl.pos.y + 16;
+        var sleft = pl.pos.x - 16 * (canvas.width / canvas.height);
+        var sright = pl.pos.x + 16 * (canvas.width / canvas.height);
+        for (var i = 0; i < 50; i++)
+            for (var j = 0; j < 50; j++) {
+                if (!(i * 20 > sright ||
+                    (i + 1) * 20 < sleft ||
+                    j * 20 > sbottom ||
+                    (j + 1) * 20 < stop)) {
+                    gl.bindBuffer(gl.ARRAY_BUFFER, this.chunks.g(i, j).vbo);
+                    var t = gl.getUniformLocation(this.shader, 'i');
+                    gl.vertexAttribPointer(t, 4, gl.FLOAT, false, 0, 0);
+                    gl.enableVertexAttribArray(t);
+                    // gl.vertexAttribPointer(t2, 2, gl.FLOAT, false, 8*4, 8*2);
+                    // gl.enableVertexAttribArray(t2);
+                    gl.drawArrays(gl.TRIANGLES, 0, this.chunks.g(i, j).vsize);
+                }
+            }
         gl.bindBuffer(gl.ARRAY_BUFFER, null);
         gl.useProgram(null);
     };
